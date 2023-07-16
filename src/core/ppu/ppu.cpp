0f761d14aa02693c835863d7974d7c2b69601791
vrgkar <viraajraulgkar@gmail.com>
2023-07-16 13:58:49 +0400

    PPU: Added basic fetch/commit functionality

--- src/core/ppu/ppu.cpp
+++ src/core/ppu/ppu.cpp
@@ -2,20 +2,101 @@
 
 bool PPU::fetch(uint8_t &byte, uint16_t addr)
 {
-    if (0x2000u <= addr && addr < 0x4000u)
-        ;
-    else
+    /*
+    Reading any readable PPU port fills the PPU Latch
+    Reading from a nominally write-only PPU port returns the current value of the latch.
+    */
+    if (addr == 0x4014u)
+    {
+        byte = m_r.ppulatch;
+        return true;
+    }
+
+    switch (addr % 0x8u)
+    {
+    case 0x0u:
+        byte = m_r.ppulatch;
+        break;
+    case 0x1u:
+        byte = m_r.ppulatch;
+        break;
+    case 0x2u:
+        byte = m_r.ppustatus;
+        m_r.ppulatch = byte;
+        break;
+    case 0x3u:
+        byte = m_r.ppulatch;
+        break;
+    case 0x4u:
+        byte = m_r.oamdata;
+        m_r.ppulatch = byte;
+        break;
+    case 0x5u:
+        byte = m_r.ppuscroll;
+        m_r.ppulatch = byte;
+        break;
+    case 0x6u:
+        byte = m_r.ppulatch;
+        break;
+    case 0x7u:
+        byte = m_r.ppudata;
+        m_r.ppulatch = byte;
+        break;
+    default:
         return false;
+    }
 
     return true;
 }
 
 bool PPU::commit(uint8_t byte, uint16_t addr)
 {
-    if (0x2000u <= addr && addr < 0x4000u)
-        ;
-    else
-        return false;
+    /* Writes to any PPU port will also fill the bits in the latch */
 
+    if (addr == 0x4014u)
+    {
+        m_r.oamdma = byte;
+        m_r.ppulatch = byte;
+
+        // Broadcast OAMDMA 
+
+        return true;
+    }
+
+    switch (addr % 0x8u)
+    {
+    case 0x0u:
+        m_r.ppuctrl = byte;
+        break;
+    case 0x1u:
+        m_r.ppumask = byte;
+        break;
+    case 0x2u:
+        break;
+    case 0x3u:
+        m_r.oamaddr = byte;
+        break;
+    case 0x4u:
+        m_r.oamdata = byte;
+        break;
+    case 0x5u:
+        m_r.ppuscroll = byte;
+        break;
+    case 0x6u:
+        m_r.ppuaddr = byte;
+        break;
+    case 0x7u:
+        m_r.ppudata = byte;
+        break;
+    default:
+        return false;
+    }
+
+    m_r.ppulatch = byte;
     return true;
+}
+
+void PPU::execute()
+{
+
 }
--- src/core/ppu/ppu.h
+++ src/core/ppu/ppu.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <array>
+
 #include "../system/component/component.h"
+#include "ppuregister/ppuregister.h"
+
+#include "../bus/bus.h"
 
 class PPU final : public Component
 {
@@ -8,5 +13,18 @@ public:
     bool fetch(uint8_t &byte, uint16_t addr) override;
     bool commit(uint8_t byte, uint16_t addr) override;
 
+    void execute();
+
     std::string_view get_id() override { return "PPU"; }
+
+private:
+
+    /* Common Bus */
+    Bus *bus = nullptr;
+
+    /* Internal PPU Registers */
+    PPURegister m_r;
+
+    /* Internal OAM Memory */
+    std::array<uint8_t, 64*4> m_oam;
 };
