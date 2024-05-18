ae7f42b9ec5b001d007b42fb647a332c18d8e762
vrgkar <viraajraulgkar@gmail.com>
2024-05-18 06:06:20 +0530

    Core: Rename component class member functions

--- src/core/component.h
+++ src/core/component.h
@@ -8,17 +8,16 @@
 class Component
 {
 public:
-    /* Fetches a value into byte from internal memory,
-       and returns whether byte was fetched or not */
+    /* Reads a value into byte from internal memory,
+       and returns whether byte was read or not */
+    virtual bool read(uint8_t &byte, uint16_t addr) = 0;
 
-    virtual bool fetch(uint8_t &byte, uint16_t addr) = 0;
-
-    /* Commits to an internal address, 
-       and returns whether the value was committed or not */
-    virtual bool commit(uint8_t byte, uint16_t addr) = 0;
+    /* Writes to an internal address, 
+       and returns whether the value was written to or not */
+    virtual bool write(uint8_t byte, uint16_t addr) = 0;
 
     /* Returns the ID of the component */
-    virtual std::string_view get_id() = 0;
+    virtual std::string_view get_id() const = 0;
 
     virtual void broadcast(Event event);
     virtual void service(Event event);
