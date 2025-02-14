e1ab8567cdac79e6f2a2d604c1dec7539db87115
Viraaj Raulgaonkar <viraajraulgkar@gmail.com>
2025-02-14 22:51:47 +0530

    core/common: Update SEGMENT defines in defs.h

--- src/core/common/defs.h
+++ src/core/common/defs.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#define BEGIN_CYCLE {
-#define STEP_CYCLE(a,b) } if (a == b) {
-#define END_CYCLE }
+#define INIT_SEGMENTS static int __segment_number = 0;
+#define START_SEGMENT(n) if (__segment_number < n) { __segment_number = n;
+#define END_SEGMENT return false; }
+#define NEXT_SEGMENT(n) END_SEGMENT else START_SEGMENT(n)
+#define BREAK_SEGMENT(n) START_SEGMENT(n) END_SEGMENT
+#define FREE_SEGMENTS __segment_number = 0;
 
