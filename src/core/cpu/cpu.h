7680c9e411da77f2285cb41f3ef654f57768cce2
Viraaj Raulgaonkar <viraajraulgkar@gmail.com>
2025-02-14 16:58:27 +0530

    core/cpu: add declarations to cpu.h
    The CPU module is unnecessarily complicated and inefficient due to the
    instruction, interrupt, and addressing mode logic being encapsulated as
    classes. This commit marks the transition to moving all of the CPU logic
    in the CPU class.

--- src/core/cpu/cpu.h
+++ src/core/cpu/cpu.h
@@ -43,4 +43,84 @@ private:
     CPUState m_state;
     Bus *m_bus = nullptr;
     std::string m_instr;
+
+private:
+
+    /* Instructions */
+    bool ADC();
+    bool AND();
+    bool ASL();
+    bool BCC();
+    bool BCS();
+    bool BEQ();
+    bool BIT();
+    bool BMI();
+    bool BNE();
+    bool BPL();
+    bool BRK();
+    bool BVC();
+    bool BVS();
+    bool CLC();
+    bool CLD();
+    bool CLI();
+    bool CLV();
+    bool CMP();
+    bool CPX();
+    bool CPY();
+    bool DEC();
+    bool DEX();
+    bool DEY();
+    bool EOR();
+    bool INC();
+    bool INX();
+    bool INY();
+    bool JMP();
+    bool JSR();
+    bool LDA();
+    bool LDX();
+    bool LDY();
+    bool LSR();
+    bool NOP();
+    bool ORA();
+    bool PHA();
+    bool PHP();
+    bool PLA();
+    bool PLP();
+    bool ROL();
+    bool ROR();
+    bool RTI();
+    bool RTS();
+    bool SBC();
+    bool SEC();
+    bool SED();
+    bool SEI();
+    bool STA();
+    bool STX();
+    bool STY();
+    bool TAX();
+    bool TAY();
+    bool TSX();
+    bool TXA();
+    bool TXS();
+    bool TYA();
+
+    /* Modes */
+    bool IMP();
+    bool IMM();
+    bool ZPG();
+    bool ZPX();
+    bool ZPY();
+    bool ACC();
+    bool REL();
+    bool ABS();
+    bool ABX();
+    bool ABY();
+    bool IDX();
+    bool IDY();
+
+    /* Interrupts */
+    bool RST();
+    bool IRQ();
+    bool NMI();
+
 };
