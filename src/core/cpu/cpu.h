9f14ba7f233e4e217aa9728db6cbd0c914faf800
Viraaj Raulgaonkar <viraajraulgkar@gmail.com>
2025-02-15 11:58:01 +0530

    core/cpu: move CPU state into the CPU class

--- src/core/cpu/cpu.h
+++ src/core/cpu/cpu.h
@@ -6,12 +6,9 @@
 
 #include "bus/bus.h"
 
-#include "cpustate.h"
-
 class CPU final : public Component
 {
 public:
-
     /* Component Interface */
     bool read(uint8_t &byte, uint16_t addr) override;
     bool write(uint8_t byte, uint16_t addr) override;
@@ -31,18 +28,158 @@ public:
     void set_bus(Bus *bus) { m_bus = bus; }
 
 private:
+    struct Register
+    {
+        enum Status : unsigned
+        {
+            C,
+            Z,
+            I,
+            D,
+            B,
+            X,
+            V,
+            N
+        };
+        
+        void set(Status f, bool b)  { sr = (sr & ~(1u << f)) | (b << f); }
+        bool test(Status f)         { return sr & (1u << f); }
+        void reset()                { sp -= 3u; set(I, true); }
+
+        /* Accumulator Register */
+        uint8_t a = 0x0u;
+    
+        /* Index Registers */
+        uint8_t x = 0x0u;
+        uint8_t y = 0x0u;
+    
+        /* Program Counter */
+        uint16_t pc = 0xC000u;
+    
+        /* Stack Pointer */
+        uint8_t sp = 0xFDu;
+    
+        /* Status Register */
+        uint8_t sr = 0x24u;
+    
+        /* Memory Address Register */
+        uint16_t mar;
+    
+        /* Memory Data Register */
+        uint8_t mdr;        
+        
+    } m_r;
+    
+    enum class State
+    {
+        FETCH,
+        DECODE,
+        EXECUTE_MODE,
+        EXECUTE_INSTRUCTION,
+        INTERRUPT
+    } m_state = State::FETCH;
+    
+    enum class Mode
+    {
+        IMP,
+        IMM,
+        ZPG,
+        ZPX,
+        ZPY,
+        ACC,
+        REL,
+        ABS,
+        ABX,
+        ABY,
+        IND,
+        IDX,
+        IDY
+    } m_mode = Mode::IMP;
+
+    enum class Instruction
+    {
+        ADC,
+        AND,
+        ASL,
+        BCC,
+        BCS,
+        BEQ,
+        BIT,
+        BMI,
+        BNE,
+        BPL,
+        BRK,
+        BVC,
+        BVS,
+        CLC,
+        CLD,
+        CLI,
+        CLV,
+        CMP,
+        CPX,
+        CPY,
+        DEC,
+        DEX,
+        DEY,
+        EOR,
+        INC,
+        INX,
+        INY,
+        JMP,
+        JSR,
+        LDA,
+        LDX,
+        LDY,
+        LSR,
+        NOP,
+        ORA,
+        PHA,
+        PHP,
+        PLA,
+        PLP,
+        ROL,
+        ROR,
+        RTI,
+        RTS,
+        SBC,
+        SEC,
+        SED,
+        SEI,
+        STA,
+        STX,
+        STY,
+        TAX,
+        TAY,
+        TSX,
+        TXA,
+        TXS,
+        TYA
+    } m_instr = Instruction::NOP;
+
+    enum class Interrupt
+    {
+        RST,
+        IRQ,
+        NMI
+    } m_interrupt = Interrupt::RST;
+
+    /* Opcode of current instruction */
+    uint8_t m_opcode;
+
+    /* Cycles Elapsed */
+    int m_cycles = 0;
+
+    /* Bus Reference */
+    Bus *m_bus = nullptr;
+
+private:
+
+    /* States */
     void fetch();
     void decode();
-    void execute();
+    void execute_mode();
+    void execute_instruction();
     void interrupt();
-
-private:
-
-    CPUState m_state;
-    Bus *m_bus = nullptr;
-    std::string m_instr;
-
-private:
 
     /* Instructions */
     bool ADC();
@@ -113,6 +250,7 @@ private:
     bool ABS();
     bool ABX();
     bool ABY();
+    bool IND();
     bool IDX();
     bool IDY();
 
@@ -120,5 +258,4 @@ private:
     bool RST();
     bool IRQ();
     bool NMI();
-
 };
