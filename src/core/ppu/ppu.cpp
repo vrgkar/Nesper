3612cf427c01abd98855ceffaec9143e50daf6bb
Viraaj Raulgaonkar <viraajraulgkar@gmail.com>
2025-03-13 15:59:56 +0530

    core/ppu: implement PPU module
    This is a preliminary implementation of the PPU component, with both
    foregrond and background processes implemented.

--- src/core/ppu/ppu.cpp
+++ src/core/ppu/ppu.cpp
@@ -1,98 +1,569 @@
 #include "ppu.h"
 
-bool PPU::read(uint8_t &byte, uint16_t addr)
+uint8_t PPU::read(uint16_t addr)
 {
-    if (addr >= 0x4000u)
-        return false;
-
-    switch (addr % 0x8u)
+    uint8_t byte = 0;
+    switch (addr % 8)
     {
-    case 0x0u:
-        byte = m_state.r.ppulatch;
+    /* PPUCTRL */
+    case 0:
+        byte = m_ppulatch;
         break;
-    case 0x1u:
-        byte = m_state.r.ppulatch;
+    /* PPUMASK */
+    case 1:
+        byte = m_ppulatch;
         break;
-    case 0x2u:
-        byte = m_state.r.ppustatus;
-        m_state.r.ppulatch = byte;
+    /* PPUSTATUS */
+    case 2:
+        byte = (m_vblank_mode << 7) | (m_sprite_zero_hit << 6) | (m_sprite_overflow << 5);
+
+        /* Clear VBlank on read */
+        m_vblank_mode = 0;
+
+        /* Clear w on read */
+        m_w = 0;
+
+        m_ppulatch = byte;
         break;
-    case 0x3u:
-        byte = m_state.r.ppulatch;
+    /* OAMADDR */
+    case 3:
+        byte = m_ppulatch;
         break;
-    case 0x4u:
-        byte = m_state.r.oamdata;
-        m_state.r.ppulatch = byte;
+    /* OAMDATA */
+    case 4:
+        /* This happens *only* during VBlank, but it's good enough for now */
+        byte = m_oam[m_oamaddr];
+
+        m_ppulatch = byte;
         break;
-    case 0x5u:
-        byte = m_state.r.ppuscroll;
-        m_state.r.ppulatch = byte;
+    /* PPUSCROLL */
+    case 5:
+        byte = m_ppulatch;
         break;
-    case 0x6u:
-        byte = m_state.r.ppulatch;
+    /* PPUADDR */
+    case 6:
+        byte = m_ppulatch;
         break;
-    case 0x7u:
-        byte = m_state.r.ppudata;
-        m_state.r.ppulatch = byte;
+    /* PPUDATA */
+    case 7:
+        /*  This only applies OUTSIDE of rendering */
+
+        byte = m_ppubuffer;
+        if (m_v < 0x3F00)
+            m_ppubuffer = m_bus->read_ppu(m_v);
+        else
+            byte = m_ppubuffer = read_palette_ram(m_v);
+
+        m_v += m_inc_mode ? 32 : 1;
+        m_ppulatch = byte;
         break;
-    default:
-        return false;
     }
 
-    return true;
+    return byte;
 }
 
-bool PPU::write(uint8_t byte, uint16_t addr)
+void PPU::write(uint8_t byte, uint16_t addr)
 {
-    /* Writes to any PPU port will also fill the bits in the latch */
+    /* Writes to any PPU port will also fill the bits  in the latch */
 
-    if (addr >= 0x4000u)
-        return false;
+    m_ppulatch = byte;
     
-    switch (addr % 0x8u)
+    switch (addr % 8)
     {
-    case 0x0u:
-        m_state.r.ppuctrl = byte;
+    case 0:                                           /* PPUCTRL */
+    /* Turning NMI enable on while the vblank is 1 triggers NMI */
+        if (!m_reset)
+        {
+            m_nmi_enable            = (byte & 0x80) > 0;
+            m_master_slave          = (byte & 0x40) > 0;
+            m_sprite_height         = (byte & 0x20) > 0;
+            m_bg_table              = (byte & 0x10) > 0;
+            m_sprite_table          = (byte & 0x08) > 0;
+            m_inc_mode              = (byte & 0x04) > 0;
+
+            m_t = (m_t & 0x73FF) | ((byte & 0x03) << 10);
+        }
         break;
-    case 0x1u:
-        m_state.r.ppumask = byte;
+    /* PPUMASK */
+    case 1:
+        /* Toggling rendering takes 3-4 cycles after write. */
+        if (!m_reset)
+        {
+            m_sprite_enable         = (byte & 0x10) > 0;
+            m_bg_enable             = (byte & 0x08) > 0;
+            m_sprite_left_enable    = (byte & 0x04) > 0;
+            m_bg_left_enable        = (byte & 0x02) > 0;
+            m_grayscale_enable      = (byte & 0x01) > 0;
+        }
         break;
-    case 0x2u:
+
+    case 2:                                       /* PPUSTATUS */
         break;
-    case 0x3u:
-        m_state.r.oamaddr = byte;
+
+    case 3:                                       /* OAMADDR */
+        m_oamaddr = byte;
         break;
-    case 0x4u:
-        m_state.r.oamdata = byte;
+    case 4:                                       /* OAMDATA */
+        if (m_scanline > 239 || m_scanline == -1)  
+        {
+            m_oam[m_oamaddr] = byte;
+            if (++m_oamaddr == 256)
+                m_oamaddr = 0;
+        }
+
         break;
-    case 0x5u:
-        m_state.r.ppuscroll = byte;
+    case 5:                                       /* PPUSCROLL */
+        if (m_w)
+        {
+            m_t &= 0x0C1F;
+            m_t |= (byte & 0x07) << 12;
+            m_t |= (byte & 0xF8) << 2;
+            m_w = 0;
+        }
+        else
+        {
+            m_t = (m_t & 0x7FE0) | (byte >> 3);
+            m_x = byte & 0x07;
+            m_w = 1;
+        }
         break;
-    case 0x6u:
-        m_state.r.ppuaddr = byte;
+    case 6:                                        /* PPUADDR */
+        if (m_w)
+        {
+            m_t = (m_t & 0x7F00) | byte;
+            m_v = m_t;
+            m_w = 0;
+        }
+        else
+        {
+            m_t = (m_t & 0x00FF) | ((byte & 0x3F) << 8);
+            m_w = 1;
+        }
         break;
-    case 0x7u:
-        m_state.r.ppudata = byte;
+    case 7:
+        if (m_v < 0x3F00u)
+            m_bus->write_ppu(byte, m_v);          /* This only applies OUTSIDE of rendering */
+        else
+            write_palette_ram(byte, m_v);
+        m_v += m_inc_mode ? 32 : 1;
         break;
-    default:
-        return false;
     }
 
-    m_state.r.ppulatch = byte;
-    return true;
 }
 
-void PPU::broadcast(Event event)
+uint8_t PPU::read_palette_ram(uint16_t addr)
 {
+    addr &= 0x001F;
+            
+    if (addr == 0x0010) addr = 0x0000;
+    if (addr == 0x0014) addr = 0x0004;
+    if (addr == 0x0018) addr = 0x0008;
+    if (addr == 0x001C) addr = 0x000C;
 
+    return m_palette_ram[addr] & (m_grayscale_enable ? 0x30 : 0x3F);    
 }
 
-void PPU::service(Event event)
+void PPU::write_palette_ram(uint8_t byte, uint16_t addr)
 {
+    addr &= 0x001F;
+            
+    if (addr == 0x0010) addr = 0x0000;
+    if (addr == 0x0014) addr = 0x0004;
+    if (addr == 0x0018) addr = 0x0008;
+    if (addr == 0x001C) addr = 0x000C;
 
+    m_palette_ram[addr] = byte; 
 }
 
 void PPU::step()
 {
+    update();
 
+    if (m_scanline == -1)
+    {
+        update_prerender();
+        
+        background_fetch();
+    }
+    else if (m_scanline < 240)
+    {
+        update_render();
+
+        background_fetch();
+
+        secondary_oam_clear();
+        sprite_evaluation();
+        sprite_fetch();
+
+        background_render();
+        sprite_render();
+        
+        render();
+    }
+    else
+        update_postrender();
+    
+    clock();
+}
+
+void PPU::update()
+{}
+
+void PPU::update_prerender()
+{
+    m_reset = false;
+
+    if (m_cycle == 1)
+        m_vblank_mode = m_sprite_zero_hit = m_sprite_overflow = 0;
+
+    else if (279 < m_cycle && m_cycle < 305)
+        transfer_y();
+
+    else if (m_cycle == 340)
+        m_odd_frame = !m_odd_frame;
+
+}
+
+void PPU::update_render()
+{
+    if (m_bg_enable && m_cycle == 0 && m_scanline == 0 && m_odd_frame)
+        m_cycle = 1;
+    
+    if (m_cycle == 338 || m_cycle == 340)
+      m_nt = m_bus->read_ppu(0x2000 | (m_v & 0x0FFF));
+}
+
+void PPU::update_postrender()
+{
+    if (m_cycle == 1 && m_scanline == 241)
+    {
+        m_vblank_mode = 1;
+        if (m_nmi_enable)
+            m_bus->broadcast(this, Event::NMI);
+    }    
+}
+
+void PPU::clock()
+{
+    if (++m_cycle == 341)
+    {
+        m_cycle = 0;
+        
+        if (++m_scanline == 261)
+            m_scanline = -1;
+    }
+}
+
+void PPU::background_fetch()
+{
+    if (!((m_cycle > 0 && m_cycle < 258) || (m_cycle > 320 && m_cycle < 338)))
+        return;
+
+    update_shifters();
+
+    int addr = 0;
+        
+    switch ((m_cycle - 1) % 8)
+    {
+    case 0:
+        reload_shifters();
+        addr = 0x2000 | (m_v & 0x0FFF);
+        m_nt = m_bus->read_ppu(addr);
+        break;
+    
+    case 2:
+        addr = 0x23C0 | (m_v & 0x0C00) | ((m_v >> 4) & 0x38) | ((m_v >> 2) & 0x07);        
+        m_at = m_bus->read_ppu(addr);
+    
+        if (m_v & 0x02)
+            m_at >>= 2;
+        if ((m_v >> 5) & 0x02)
+            m_at >>= 4;
+
+        m_at &= 0x03;
+        break;
+
+    case 4:
+        addr = (m_bg_table << 12) + (m_nt << 4) + (m_v >> 12);
+        m_pt_lo = m_bus->read_ppu(addr);
+        break;
+
+    case 6:
+        addr = (m_bg_table << 12) + (m_nt << 4) + (m_v >> 12) + 8;
+        m_pt_hi = m_bus->read_ppu(addr);
+        break;
+        
+    case 7:
+        increment_scroll_x();
+        break;
+    }
+
+    if (m_cycle == 256)
+        increment_scroll_y();
+
+    if (m_cycle == 257)
+    {
+        reload_shifters();
+        transfer_x();
+    }
+}
+
+void PPU::secondary_oam_clear()
+{
+    if (0 < m_cycle && m_cycle < 65)
+        m_secoam[m_cycle % m_secoam.size()] = m_oamdata = 0xFF;
+}
+
+bool PPU::sprite_evaluation()
+{
+    if (m_cycle < 65 || m_cycle > 256)
+        return false;
+
+    INIT_SEGMENTS
+
+    static int n = 0, m = 0;    /* Primary OAM Indices */
+    static int k = 0;           /* Secondary OAM Index */
+
+    if (m_cycle % 2)
+    {
+        m_oamdata = m_oam[4*n+m];
+        return false;
+    }
+    else
+    {
+        START_SEGMENT(1)
+
+        m_secoam[k % m_secoam.size()] = m_oamdata;
+        
+        if (m_oamdata <= m_scanline && m_scanline < m_oamdata + (m_sprite_height ? 16 : 8))
+        {
+            m = (m+1) % 4;
+            ++k;
+        }
+        else
+        {
+            n = (n+1) % 64;
+            
+            if (n == 0)
+                GOTO_SEGMENT(5)
+            else
+                REPEAT_SEGMENT
+        }
+        
+        NEXT_SEGMENT(2)
+        
+        m_secoam[k % m_secoam.size()] = m_oamdata;
+
+        ++k;
+        m = (m+1) % 4;
+
+        if (m == 0)
+        {
+            n = (n+1) % 64;
+
+            if (n == 0)
+                GOTO_SEGMENT(5)
+            
+            else if (k < m_secoam.size())
+                GOTO_SEGMENT(1)
+        }
+        else
+            REPEAT_SEGMENT;
+
+        NEXT_SEGMENT(3)
+
+        if (!(m_oamdata <= m_scanline && m_scanline < m_oamdata + (m_sprite_height ? 16 : 8)))
+        {
+            m = (m+1) % 4;
+            n = (n+1) % 64;
+
+            if (n == 0)
+                GOTO_SEGMENT(5)
+            else
+                REPEAT_SEGMENT
+        }
+        else
+        {
+            m_sprite_overflow = 1;
+            m = (m+1) % 4;
+        }
+
+        NEXT_SEGMENT(4)
+
+        m = (m+1) % 4;
+
+        if (m == 0)
+        {
+            n = (n+1) % 64;
+            if (n != 0)
+                GOTO_SEGMENT(3)
+        }
+        else
+            REPEAT_SEGMENT
+
+        END_SEGMENT
+
+        n = (n+1) % 64;
+
+        if (m_cycle < 256)
+            return false;
+    }
+
+    n = m = k = 0;
+    FREE_SEGMENTS
+    return true;
+}
+
+void PPU::sprite_fetch()
+{
+    if (m_cycle == 320)
+    {
+        for (int i = 0; i < 8; ++i)
+        {
+            m_sprites[i].y = m_secoam[4*i]+1;
+            m_sprites[i].tile = m_secoam[4*i+1];
+            m_sprites[i].flip_v = m_secoam[4*i+2] & 0x80;
+            m_sprites[i].flip_h = m_secoam[4*i+2] & 0x40;
+            m_sprites[i].priority = m_secoam[4*i+2] & 0x20;
+            m_sprites[i].pal_hi = m_secoam[4*i+2] & 0x03;
+            m_sprites[i].x = m_secoam[4*i+3];
+        }
+    }
+}
+
+void PPU::background_render()
+{
+    m_bg_pal = 0;
+
+    if (!m_bg_enable || m_cycle < (m_bg_left_enable ? 1 : 8) || m_cycle > 256)
+        return;
+
+    auto mux = 0x8000 >> m_x;
+
+    auto bg_pal_lo_lsb = (m_pt_lsb & mux) ? 1 : 0;
+    auto bg_pal_lo_msb = (m_pt_msb & mux) ? 1 : 0;
+
+    auto bg_pal_lo = (bg_pal_lo_msb << 1) | bg_pal_lo_lsb;
+
+    auto bg_pal_hi_lsb = (m_at_lsb & mux) ? 1 : 0;
+    auto bg_pal_hi_msb = (m_at_msb & mux) ? 1 : 0;
+
+    auto bg_pal_hi = (bg_pal_hi_msb << 1) | bg_pal_hi_lsb;
+
+    m_bg_pal = (bg_pal_hi << 2) | bg_pal_lo;
+
+    m_bg_pal &= 0x001F;
+
+    if (m_bg_pal == 0x0010) m_bg_pal = 0x0000;
+    if (m_bg_pal == 0x0014) m_bg_pal = 0x0004;
+    if (m_bg_pal == 0x0018) m_bg_pal = 0x0008;
+    if (m_bg_pal == 0x001C) m_bg_pal = 0x000C;
+
+}
+
+void PPU::sprite_render()
+{
+    m_sprite_pal = 0;
+
+    if (m_sprite_enable && m_scanline != 0)
+    {
+        for (m_sprite_idx = 0; m_sprite_idx < 8; ++m_sprite_idx)
+        {
+            const auto &sprite = m_sprites[m_sprite_idx];
+
+            if (sprite.y <= m_scanline && m_scanline < sprite.y + (m_sprite_height ? 16 : 8))
+                if (sprite.x <= m_cycle && m_cycle < sprite.x + 8)
+                    break;
+        }
+        
+        if (m_sprite_idx < 8)
+        {
+            const auto &sprite = m_sprites[m_sprite_idx];
+
+            auto addr = 0;
+
+            if (m_sprite_height == 0)
+            {
+                if (sprite.flip_v)
+                    addr = (m_sprite_table << 12) | (sprite.tile << 4) | (7 - (m_scanline - sprite.y));
+                else
+                    addr = (m_sprite_table << 12) | (sprite.tile << 4) | (m_scanline - sprite.y);
+
+            }
+            else
+            {
+                if (sprite.flip_v)
+                {
+                    if (m_scanline - sprite.y < 8)
+                        addr = ((sprite.tile & 1) << 12) | (sprite.tile | 0x01) << 4 | (7 - (m_scanline - sprite.y));                    
+                    else
+                        addr = ((sprite.tile & 1) << 12) | (sprite.tile & 0xFE) << 4 | (7 - (m_scanline - sprite.y - 8));
+                }
+                else
+                {
+                    if (m_scanline - sprite.y < 8)
+                        addr = ((sprite.tile & 1) << 12) | (sprite.tile & 0xFE) << 4 | (m_scanline - sprite.y);
+                    else
+                        addr = ((sprite.tile & 1) << 12) | (sprite.tile | 0x01) << 4 | (m_scanline - sprite.y - 8);
+                }
+            }
+
+            auto spr_pt_lo = m_bus->read_ppu(addr);
+            auto spr_pt_hi = m_bus->read_ppu(addr + 8);
+
+            auto mux = 0x80;
+
+            if (sprite.flip_h)
+                mux >>= 7 - (m_cycle - sprite.x);
+            else
+                mux >>= m_cycle - sprite.x;
+
+            auto spr_pal_lo_lsb = (spr_pt_lo & mux) ? 1 : 0;
+            auto spr_pal_lo_msb = (spr_pt_hi & mux) ? 1 : 0;
+
+            auto spr_pal_lo = (spr_pal_lo_msb << 1) | spr_pal_lo_lsb;
+
+            m_sprite_pal = 0x10 | (sprite.pal_hi << 2) | spr_pal_lo;
+
+            m_sprite_pal &= 0x001F;
+        
+            if (m_sprite_pal == 0x0010) m_sprite_pal = 0x0000;
+            if (m_sprite_pal == 0x0014) m_sprite_pal = 0x0004;
+            if (m_sprite_pal == 0x0018) m_sprite_pal = 0x0008;
+            if (m_sprite_pal == 0x001C) m_sprite_pal = 0x000C;
+        }
+    }
+    
+    if (m_sprite_idx == 0 && m_sprite_enable && m_bg_enable && 
+        (m_sprite_left_enable ? 0 : 8) < m_cycle && m_cycle < 256)
+    {
+        m_sprite_zero_hit |= ((m_sprite_pal & 0x0F) % 4) && ((m_bg_pal & 0x0F) % 4);
+    }
+}
+
+void PPU::render()
+{
+    if (!(0 < m_cycle && m_cycle < 257))
+        return;
+
+    auto bg_col = read_palette_ram(m_bg_pal);
+    auto spr_col = read_palette_ram(m_sprite_pal);
+
+    auto col = read_palette_ram(0);
+
+    if ((m_bg_pal & 0x0F) % 4 && (m_sprite_pal & 0x0F) % 4)
+    {
+        if (m_sprites[m_sprite_idx].priority)
+            col = bg_col;
+        else
+            col = spr_col;
+    }
+    else if ((m_bg_pal & 0x0F) % 4)
+        col = bg_col;
+
+    else if ((m_sprite_pal & 0x0F) % 4)
+        col = spr_col;
+
+    for (int i = 0; i < 3; ++i)
+        m_framebuffer[m_scanline][m_cycle-1][i] = m_palette_map[3*col+i];
 }
--- src/core/ppu/ppu.h
+++ src/core/ppu/ppu.h
@@ -2,34 +2,262 @@
 
 #include "common/pch.h"
 #include "common/component.h"
+#include "common/defs.h"
 
 #include "bus/bus.h"
-
-#include "ppustate.h"
 
 class PPU final : public Component
 {
 public:
-    bool read(uint8_t &byte, uint16_t addr) override;
-    bool write(uint8_t byte, uint16_t addr) override;
-
-    void broadcast(Event event) override;
-    void service(Event event) override;
+    uint8_t read(uint16_t addr) override;
+    void write(uint8_t byte, uint16_t addr) override;
 
     void step();
 
-    void set_bus(Bus *bus) { m_bus = bus; }
+    void connect(Bus *bus) { m_bus = bus; m_bus->connect(this); }
 
     std::string_view get_id() const override { return "PPU"; }
+    bool frame_ready() { return m_cycle == 340 && m_scanline == -1; }
+
+    unsigned char* framebuffer() { return &m_framebuffer[0][0][0]; }
 
 private:
 
-    /* Common Bus */
+    enum State
+    {
+        PRERENDER,
+        RENDER,
+        POSTRENDER
+    } m_state = RENDER;
+
     Bus *m_bus = nullptr;
 
-    /* Internal PPU State */
-    PPUState m_state;
+    int m_cycle = 0;
+    int m_scanline = 0;
 
-    /* Internal OAM Memory */
-    std::array<uint8_t, 64*4> m_oam;
+    bool m_reset = true;
+    bool m_odd_frame = false;
+
+    std::array<uint8_t, 256> m_oam = {};
+    std::array<uint8_t, 32> m_secoam = {};
+    
+    std::array<uint8_t, 32> m_palette_ram = {};
+    std::array<std::array<std::array<uint8_t, 3>, 256>, 240> m_framebuffer = {};
+
+    /* Pallete Map */
+    std::array<uint8_t, 192> m_palette_map = 
+    {
+        84u, 84u, 84u,
+        0u, 30u, 116u,
+        8u, 16u, 144u,
+        48u, 0u, 136u,
+        68u, 0u, 100u,
+        92u, 0u, 48u,
+        84u, 4u, 0u,
+        60u, 24u, 0u,
+        32u, 42u, 0u,
+        8u, 58u, 0u,
+        0u, 64u, 0u,
+        0u, 60u, 0u,
+        0u, 50u, 60u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+        152u, 150u, 152u,
+        8u, 76u, 196u,
+        48u, 50u, 236u,
+        92u, 30u, 228u,
+        136u, 20u, 176u,
+        160u, 20u, 100u,
+        152u, 34u, 32u,
+        120u, 60u, 0u,
+        84u, 90u, 0u,
+        40u, 114u, 0u,
+        8u, 124u, 0u,
+        0u, 118u, 40u,
+        0u, 102u, 120u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+        236u, 238u, 236u,
+        76u, 154u, 236u,
+        120u, 124u, 236u,
+        176u, 98u, 236u,
+        228u, 84u, 236u,
+        236u, 88u, 180u,
+        236u, 106u, 100u,
+        212u, 136u, 32u,
+        160u, 170u, 0u,
+        116u, 196u, 0u,
+        76u, 208u, 32u,
+        56u, 204u, 108u,
+        56u, 180u, 204u,
+        60u, 60u, 60u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+        236u, 238u, 236u,
+        168u, 204u, 236u,
+        188u, 188u, 236u,
+        212u, 178u, 236u,
+        236u, 174u, 236u,
+        236u, 174u, 212u,
+        236u, 180u, 176u,
+        228u, 196u, 144u,
+        204u, 210u, 120u,
+        180u, 222u, 120u,
+        168u, 226u, 144u,
+        152u, 226u, 180u,
+        160u, 214u, 228u,
+        160u, 162u, 160u,
+        0u, 0u, 0u,
+        0u, 0u, 0u,
+    };
+
+    int m_nmi_enable = 0;
+    int m_master_slave = 0;
+    int m_sprite_height = 0;
+    int m_bg_table = 0;
+    int m_sprite_table = 0;
+    int m_inc_mode = 0;
+
+    int m_sprite_enable = 0;
+    int m_bg_enable = 0;
+    int m_sprite_left_enable = 0;
+    int m_bg_left_enable = 0;
+    int m_grayscale_enable = 0;
+
+    int m_vblank_mode = 0;
+    int m_sprite_zero_hit = 0;
+    int m_sprite_overflow = 0;
+    
+    int m_oamaddr = 0;
+    int m_oamdata = 0;
+
+    int m_ppulatch = 0;
+    int m_ppubuffer = 0;
+    
+    int m_v = 0;
+    int m_t = 0;
+    int m_x = 0;
+    int m_w = 0;
+
+    int m_nt = 0;
+    int m_at = 0;
+    int m_pt_lo = 0;
+    int m_pt_hi = 0;       
+
+    int m_at_lsb = 0;
+    int m_at_msb = 0;
+    int m_pt_lsb = 0;
+    int m_pt_msb = 0;
+
+    int m_bg_pal = 0;
+    int m_sprite_pal = 0;
+
+    struct Sprite
+    {
+        int y;
+        int tile;
+
+        bool flip_v;
+        bool flip_h;
+        bool priority;
+
+        int pal_hi;
+
+        int x;
+    };
+
+    std::array<Sprite, 8> m_sprites = {};
+    int m_sprite_idx = 8;
+private:
+
+    void update_shifters()                  
+    { 
+        if (m_bg_enable)
+            m_at_lsb <<= 1, m_at_msb <<= 1, m_pt_lsb <<= 1, m_pt_msb <<= 1;
+    }
+
+    void reload_shifters()
+    {
+        m_at_lsb = (m_at_lsb & 0xFF00) | ((m_at & 1) ? 0x00FF : 0);
+        m_at_msb = (m_at_msb & 0xFF00) | ((m_at & 2) ? 0x00FF : 0);
+        m_pt_lsb = (m_pt_lsb & 0xFF00) | m_pt_lo;
+        m_pt_msb = (m_pt_msb & 0xFF00) | m_pt_hi;
+    }
+
+    void increment_scroll_x()
+    {   
+        if (m_sprite_enable || m_bg_enable)
+        {
+            if ((m_v & 0x001F) == 31)
+            {
+                m_v &= 0x7FE0;
+                m_v ^= (1 << 10);
+            }
+            else
+                ++m_v;
+        }
+    }
+
+    void increment_scroll_y()
+    {
+        if (m_sprite_enable || m_bg_enable)
+        {
+            if ((m_v >> 12) < 7)
+                m_v += 0x1000;
+            else
+            {
+                int y = (m_v >> 5) & 0x1F;
+
+                if (y == 29)
+                {
+                    y = 0;
+                    m_v ^= (1 << 11);
+                }
+                else if (y == 31)
+                    y = 0;
+                else
+                    ++y;
+
+                m_v = (m_v & 0x0C1F) | (y << 5);
+            }
+        }
+    }
+
+    void transfer_x()
+    {
+        if (m_sprite_enable || m_bg_enable)
+            m_v = (m_v & 0x7BE0) | (m_t & 0x041F);
+    }
+
+    void transfer_y()
+    {
+        if (m_sprite_enable || m_bg_enable)
+            m_v = (m_v & 0x041F) | (m_t & 0x7BE0);
+    }
+
+private:
+
+    uint8_t read_palette_ram(uint16_t addr);
+    void write_palette_ram(uint8_t byte, uint16_t addr);
+
+    void clock();
+
+    void update();
+    void update_prerender();
+    void update_render();
+    void update_postrender();
+
+    void render();
+
+    void background_fetch();
+    void background_render();
+
+    void secondary_oam_clear();
+    bool sprite_evaluation();
+    void sprite_fetch();
+    void sprite_render();
+
+
 };
